49e893e2edbf07c247957ed268dd32be37bd56a5
Robert Holman <lordovos@live.com>
2020-04-12 04:59:56 -0500

    Updated styling for forms.

--- public/routes.php
+++ public/routes.php
@@ -38,17 +38,6 @@ $router->mount("/admin", function () use ($router, $container) {
     $router->get("/(\w+)/(\d+)/(\d+|-\d+)", function ($action, $id, $status) use ($container) {
         return $container["userController"]->adminAction($action, $id, $status);
     });
-/*
-    $router->get("/activate/(\d+)", function ($id) use ($container) {
-        return $container["userController"]->activateGet($id);
-    });
-    $router->get("/ban/(\d+)", function ($id) use ($container) {
-        return $container["userController"]->banGet($id);
-    });
-    $router->get("/promote/(\d+)", function ($id) use ($container) {
-        return $container["userController"]->promoteGet($id);
-    });
-*/
 });
 $router->mount("/wiki", function () use ($router, $container) {
     $router->get("/", function () use ($container) {
